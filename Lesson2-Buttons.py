e351b1f0513fa4803ba5bc1791410c6bab834d64
Josh Orndorff <admin@joshorndorff.com>
2014-04-17 18:01:01 -0400

    Commented the binding so the student can uncomment it

--- Lesson2-Buttons.py
+++ Lesson2-Buttons.py
@@ -27,7 +27,7 @@ panel = wx.Panel(frame)
 btnClickMe = wx.Button(panel, label="Click Me", pos=(20,20), size=(200,20))
 
 #Make the button do something!
-btnClickMe.Bind(wx.EVT_BUTTON, OnClickMe)
+#btnClickMe.Bind(wx.EVT_BUTTON, OnClickMe)
 
 #Show the frame
 frame.Show()
@@ -41,11 +41,12 @@ app.MainLoop()
 #1. Change the position of the frame. (You've done this before, but I want you to stay sharp.)
 
 #2. Change the text on the button, and make the button taller.
-# You may also play with the button size and location.
+#   You may also play with the button size and location.
 
 #3. The button doesn't do anything. Uncomment line 30 to *Bind* the button to an event handler.
-#An event handler is just a function that runs when an event happens.
-#In this case the event handler is called OnClickMe. It is good style to start event handlers with "OnSomething"
-#Do you see the text "Yay! You clicked it" in your terminal? Great!
+#   An event handler is just a function that runs when an event happens.
+#   In this case the event handler is called OnClickMe, and the event is clicking btnClickMe.
+#   It is good style to start event handlers with "OnSomething"
+#   Do you see the text "Yay! You clicked it" in your terminal? Great!
 
 #4. Create another button and add it to my panel. Make it print some other text to the terminal.
